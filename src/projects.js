b20a7e594dd3b76bb9886326aa42640c158d491f
Erin Bernardoni <erin.bernardoni@outlook.fr>
2023-11-17 13:01:17 +0100

    Adding in progress project Shinobi

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,7 @@
 import WeegiftImg from "./assets/project_weegift.jpg"
 import RespirationImg from "./assets/project_respiration.jpg"
 import NeurocosmosImg from "./assets/project_neurocosmo.jpg"
+import ShinobiImg from "./assets/project_shinobi.jpg"
 import Weegift from "./components/Projects/Weegift"
 import Neurocosmos from "./components/Projects/Neurocosmos"
 import Respiration from "./components/Projects/Respiration"
@@ -12,6 +13,7 @@ const projects = {
     description : "Projet universitaire visant à simplifier l'organisation de cagnottes.",
     picture: WeegiftImg,
     element: <Weegift />,
+    tags : ["ux", "react"],
     sections: [
       
     ]
@@ -22,6 +24,7 @@ const projects = {
     description : "Jeu sérieux mêlant neurosciences et aide à la personne.",
     picture: NeurocosmosImg,
     element: <Neurocosmos />,
+    tags: ["ux"],
     sections: [
       { to_display: "{à_propos}", ref: "#about" },
     ]
@@ -33,6 +36,19 @@ const projects = {
     description : "Environnement virtuel à destination d'élèves de 5e, pour mieux comprendre la respiration.",
     picture: RespirationImg,
     element: <Respiration />,
+    tags : ["ux", "vr"],
+    sections: [
+      { to_display: "{à_propos}", ref: "#about" },
+    ]
+  },
+  "shinobi" : {
+    path : "shinobi-wat-aah",
+    title: "Shinobi Wat-AAH!",
+    subtitle : "JS - PHP",
+    description : "Adaptation du jeu de société Shinobi Wat-AAH! sur le site Board Game Arena",
+    picture: ShinobiImg,
+    element: <Respiration />,
+    tags : ["php"],
     sections: [
       { to_display: "{à_propos}", ref: "#about" },
     ]
