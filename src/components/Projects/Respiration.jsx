955cbf44483c40e9d257c866683a7c47c08255c6
Erin Bernardoni <erin.bernardoni@outlook.fr>
2023-11-17 18:21:55 +0100

    [Update] Respiration page content

--- src/components/Projects/Respiration.jsx
+++ src/components/Projects/Respiration.jsx
@@ -1,8 +1,141 @@
 import React from 'react'
+import { IconQuestionMark, IconBulb, IconSearch, IconLayersSubtract, IconCode, IconHierarchy2 } from '@tabler/icons-react'
+import RespirationImg from "../../assets/project_respiration.jpg"
+import ProjectCard from '../Reusable/ProjectCard/ProjectCard';
+import projects from '../../projects';
+import Cotes from "../../assets/respiration/cotes_ouvert.png";
+import Piece from "../../assets/respiration/piece_scifi.png";
+import Popup from "../../assets/respiration/popup2.png"
 
 function Respiration() {
+  const timelineSteps = [
+    {
+      icon: <IconSearch size={32} strokeWidth={1.5} />, 
+      title: 'Recherche',
+      description: String.raw`- Connaissance des utilisateurs cibles`
+    },
+    {
+      icon: <IconHierarchy2 size={32} strokeWidth={1.5} />, 
+      title: 'Définition',
+      description: String.raw`- Objectifs pédagogiques`,
+    },
+    {
+      icon: <IconBulb size={32} strokeWidth={1.5} />, 
+      title: 'Idéation',
+      description: String.raw`- Scénario
+      - User Story
+      `,
+    },
+    {
+      icon: <IconLayersSubtract size={32} strokeWidth={1.5} />, 
+      title: 'Prototypage',
+      description: String.raw`- Maquette « Low Fidelity »
+      - Tests utilisateurs
+      `,
+    },
+    {
+      icon: <IconCode size={32} strokeWidth={1.5} />,
+      title: 'Réalisation',
+      description: String.raw`- Développement
+      - Tests utilisateurs
+      `,
+    },
+  ];
+
   return (
-    <div>Respiration</div>
+    <div>
+      {/* HERO */}
+      <div className='container mt-40'>
+        <div className='relative'>
+          <img src={RespirationImg} alt='Screen from the game Neurocosmos' />
+          <div className='absolute top-0 right-0 bg-background-black/[.88] w-1/2 h-full flex flex-col justify-center px-10'>
+            <h1 className='mb-0'>La Respiration</h1>
+            <p>Conception et Développement d'un environnement virtuel permettant à des élèves de 5e de mieux comprendre la respiration.</p>
+
+            <h2 className='mb-0'>Cadre</h2>
+            <p>Projet universitaire en groupe de VR</p>
+
+            <h2 className='mb-0'>Temps alloué</h2>
+            <p>2 mois</p>
+
+            <h2 className='mb-0'>Outils</h2>
+            <p>Figma, Blender, Unity 3D</p>
+          </div>
+        </div>
+      </div>
+
+      {/* PRESENTATION */}
+      <section className='container flex justify-between gap-16'>
+        <div className='flex flex-col items-center flex-1'>
+          <IconQuestionMark size={50}/>
+          <h2>Problème</h2>
+          <p>Des outils immersifs peuvent permettre de mieux percevoir le fonctionnement de nombreuses choses, notamment du vivant. C'est pourquoi un collège à proximité de Nancy a fait appel à l'Université de Lorraine pour réaliser des applications en réalité virtuelle ou augmentée reprenant des concepts de cours.</p>
+        </div>
+
+        <div className='flex flex-col items-center flex-1'>
+          <IconBulb size={50}/>
+          <h2>Solution</h2>
+          <p>Le groupe dans lequel j'étais a réalisé une application de réalité virtuelle, permettant de répondre à la problématique de la connaissance du fonctionnement des muscles du diaphragme, des côtes et du gonflement des poumons. Designée pour un public de 5e au collège, cette application immersive permet de voir les mouvements respiratoires et d'en apprendre plus sur les différents organes.</p>
+        </div>
+      </section>
+
+      {/* REALISATION */}
+      <div className='light_background'>
+        <section className='container'>
+          <h1>Réalisation</h1>
+          <h2>Environnement 3D</h2>
+          <div className='two-cols'>
+            <div className='flex-1'>
+              <h3>Nos choix de représentation</h3>
+              <p>
+                Nous avons délibérément choisi de représenter un modèle humain en transparence, offrant ainsi une vue détaillée des poumons et de la cage thoracique. Cette approche immersive permet aux élèves de visualiser en temps réel les mécanismes complexes de la respiration, offrant une compréhension approfondie du fonctionnement du système respiratoire.
+              </p>
+              <p>
+                De manière interactive, les utilisateurs ont également la possibilité de retirer la cage thoracique, offrant ainsi une perspective encore plus détaillée des poumons en action. Cette fonctionnalité permet aux élèves de mieux saisir les relations anatomiques et de visualiser les changements internes qui se produisent lors de l'inspiration et de l'expiration.
+              </p>
+              <p>
+                En plus de l'aspect visuel, notre projet 'La Respiration' propose des affichages informatifs et des anecdotes amusantes directement intégrés à l'environnement virtuel. Ces informations supplémentaires permettent d'enrichir l'expérience éducative en fournissant des détails sur les poumons, les côtes, et d'autres aspects liés à la respiration.
+              </p>
+            </div>
+            <div className='flex-1'>
+              <img src={Cotes} alt="Modèle 3D du buste" className='max-w-xs mb-8'/>
+              <img src={Popup} alt="Popup donnant des indications sur les poumons" className='max-w-xs'/>
+            </div>
+          </div>
+
+          <div className='two-cols mt-16'>
+            <div className='flex-1'>
+              <img src={Piece} alt="Environnement virtuel 3D (pièce futuriste)" />
+            </div>
+            <div className='flex-1'>
+              <h3>Technologies utilisées</h3>
+              <p>Pour réaliser ce projet nous avons utilisé Blender pour adapter le modèle 3D à partir de modèles trouvés dans des librairie. Il était par exemple nécessaire de découper les côtes pour leur mouvement.</p>
+              <p>L'environnement virtuel a été réalisé sur Unity 3D.</p>
+            </div>
+          </div>
+
+          <h2 className='mt-16'>Tests utilisateurs</h2>
+          <p>Nous avons eu la chance de faire tester notre environnement virtuel a une classe d'élèves de 5e, ce qui nous a permis d'avoir des retours sur cette environnement ainsi que de son utilité.</p>
+          <p>Pour évaluer l'utilisabilié de l'environnement virtuel, nous avons utilisé le test System Usability Scale de Brooke. Les élèves étaient finalement plutôt satisfaits. Ils ont notamment évoqué une meilleure compréhension de l'implication et du fonctionnement des organes dans la respiration. Des problèmes dans les interactions ont aussi été relevés, ce qui donne des pistes d'amélioration à notre projet.</p>
+        </section>
+      </div>
+
+
+      <section>
+        <div className='container'>
+          <h1>Mes autres projets</h1>
+          <div className='flex gap-8 flex-wrap'>
+            {
+              Object.keys(projects)
+                .filter((key) => key !== "respiration")
+                .map((key, index) => (
+                  <ProjectCard el={projects[key]} path={key} key={key} />
+              ))
+            }
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
