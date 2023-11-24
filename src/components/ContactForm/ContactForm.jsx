58e2c732757990434860c6b5963f27adf5f36e74
Erin BERNARDONI <erin.bernardoni@outlook.fr>
2023-11-24 11:03:56 +0100

    [Contact form] uncomment sending mail

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -52,19 +52,18 @@ function ContactForm() {
       return;
     }
     else if (!formData.name) {
-      notifySucess("yep")
-      // emailjs.sendForm(
-      //   process.env.REACT_APP_EMAILJS_SERVICE_ID, 
-      //   process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
-      //   form.current, 
-      //   process.env.REACT_APP_EMAILJS_PUBLIC_KEY
-      //   )
-      //   .then((result) => {
-      //   setFormSent(true);
-      //   notifySucess("Mail envoyé, merci !");
-      // }, (error) => {
-      //   notifyError("Le mail n'a pas pu être envoyé :(");
-      // });
+      emailjs.sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID, 
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID, 
+        form.current, 
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        )
+        .then((result) => {
+        setFormSent(true);
+        notifySucess("Mail envoyé, merci !");
+      }, (error) => {
+        notifyError("Le mail n'a pas pu être envoyé :(");
+      });
     }
 
   };
